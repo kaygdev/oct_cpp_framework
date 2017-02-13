875b037c94f15309d91dde81aa20aec1b21ddff3
Kay Gawlik <kaydev@amarunet.de>
2017-02-13 21:13:03 +0100

    CallbackStepper hinzugefügt: "Automatischer" äquidistanter Callback

---
+++ cpp_framework/callback.h
@@ -0,0 +1,24 @@
+#pragma once
+
+
+namespace CppFW
+{
+
+	class Callback
+	{
+		Callback* parent = nullptr;
+		double factor = 1;
+		double add    = 0;
+
+		Callback(Callback* parent, double factor, double add) : parent(parent), factor(factor), add(add) {}
+
+	public:
+		Callback() = default;
+		virtual ~Callback()                                            {}
+		virtual bool callback(double frac)                             { if(parent) return parent->callback(frac*factor + add); return true; }
+		virtual void calcFail(int /*step*/ = -1)                       {}
+
+		Callback createSubTask(double fracSubtask, double startFrac)   { return Callback(this, fracSubtask, startFrac); }
+	};
+
+}
