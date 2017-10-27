d2b5dd7af87522c32d43d2db8715110d4ea485e9
Kay Gawlik <kay.gawlik@beuth-hochschule.de>
2017-10-27 23:01:28 +0200

    Fehlende Anpassung vorgenommen

--- cpp_framework/matcompress/simplematcompress.h
+++ cpp_framework/matcompress/simplematcompress.h
@@ -6,69 +6,72 @@
 
 #include <boost/serialization/vector.hpp>
 
-class SimpleMatCompress
+namespace CppFW
 {
-	friend class boost::serialization::access;
-	struct MatSegment
+	class SimpleMatCompress
 	{
 		friend class boost::serialization::access;
+		struct MatSegment
+		{
+			friend class boost::serialization::access;
 
-		MatSegment() {}
+			MatSegment() {}
 
-		MatSegment(int length, uint8_t value)
-		: length(length)
-		, value (value )
-		{ }
+			MatSegment(int length, uint8_t value)
+			: length(length)
+			, value (value )
+			{ }
 
-		int    length = 0;
-		uint8_t value = 0;
+			int    length = 0;
+			uint8_t value = 0;
+
+			template<class Archive>
+			void serialize(Archive & ar, const unsigned int /*version*/)
+			{
+				ar & length;
+				ar & value;
+			}
+
+			bool operator==(const MatSegment& other) const
+			{
+				return length == other.length
+				    && value  == other.value ;
+			}
+		};
+
+		std::vector<MatSegment> segmentsChange;
+		int rows = 0;
+		int cols = 0;
+		int sumSegments = 0;
 
 		template<class Archive>
 		void serialize(Archive & ar, const unsigned int /*version*/)
 		{
-			ar & length;
-			ar & value;
+			ar & rows;
+			ar & cols;
+			ar & segmentsChange;
 		}
+		void addSegment(int length, uint8_t value);
 
-		bool operator==(const MatSegment& other) const
-		{
-			return length == other.length
-			    && value  == other.value ;
-		}
+	public:
+		SimpleMatCompress() = default;
+		SimpleMatCompress(int rows, int cols, uint8_t initValue);
+
+		bool isEmpty(uint8_t defaultValue) const;
+
+		int getRows() const { return rows; }
+		int getCols() const { return cols; }
+
+		bool readFromMat(const uint8_t* mat, int rows, int cols);
+		bool writeToMat (      uint8_t* mat, int rows, int cols) const;
+
+		template<typename T>
+		bool writeToMatConvert(T* mat, int rows, int cols) const;
+
+		bool isEqual(const uint8_t* mat, int rows, int cols) const;
+		bool operator==(const SimpleMatCompress& other) const;
 	};
-
-	std::vector<MatSegment> segmentsChange;
-	int rows = 0;
-	int cols = 0;
-	int sumSegments = 0;
-
-	template<class Archive>
-	void serialize(Archive & ar, const unsigned int /*version*/)
-	{
-		ar & rows;
-		ar & cols;
-		ar & segmentsChange;
-	}
-	void addSegment(int length, uint8_t value);
-
-public:
-	SimpleMatCompress() = default;
-	SimpleMatCompress(int rows, int cols, uint8_t initValue);
-
-	bool isEmpty(uint8_t defaultValue) const;
-
-	int getRows() const { return rows; }
-	int getCols() const { return cols; }
-
-	bool readFromMat(const uint8_t* mat, int rows, int cols);
-	bool writeToMat (      uint8_t* mat, int rows, int cols) const;
-
-	template<typename T>
-	bool writeToMatConvert(T* mat, int rows, int cols) const;
-
-	bool isEqual(const uint8_t* mat, int rows, int cols) const;
-	bool operator==(const SimpleMatCompress& other) const;
-};
+}
 
 
 #endif // SIMPLEMATCOMPRESS_H
